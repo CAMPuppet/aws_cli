2fd631d86550a6c827f9be366ba9a635d1219c07
Christopher A. Moore <chris.a.moore@gmail.com>
2014-01-31 23:46:32 -0800

    add py tools

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,9 @@
 class aws_cli {
+	exec { "py_tools":
+      command => "yum -d 0 -e 0 -y install python-setuptools",
+      returns => [ 0, 1],
+    }
+
     exec{ "install_pip":
         command => "easy_install pip && pip install setuptools --no-use-wheel --upgrade",
         returns => [ 0, 1, 255 ]
